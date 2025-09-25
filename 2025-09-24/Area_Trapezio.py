3f87376ff7f8cd6ddeadf16ee56ce5a6dea83b40
JPdeAzevedoLima <jpdeazevedolima@gmail.com>
2025-09-24 21:51:47 -0300

    Alterações

--- 2025-09-24/Area_Trapezio.py
+++ 2025-09-24/Area_Trapezio.py
@@ -1,5 +1,5 @@
 #02 - Desenvolva um programa em Python que solicite ao usuário os valores da base maior, base menor e altura de um trapézio e, 
-#em seguida, calcule e exiba a sua área. Utilize a fórmula da área do trapézio. A = (B + b) * h.
+#em seguida, calcule e exiba a sua área. Utilize a fórmula da área do trapézio. A = (B + b) * h / 2.
 
 print("Vamos calcular a área de um trapezio")
 
@@ -7,6 +7,6 @@ B = float(input("Olá, por favor digite o valor da base maior: "))
 b = float(input("Olá, por favor digite o valor da base menor: "))
 h = float(input("Olá, por favor digite o valor da altura: "))
 
-area = (B + b) * h
+area = (B + b) * h / 2
 
 print(f'O valor da área do trapezio é: {area:.2f}')
