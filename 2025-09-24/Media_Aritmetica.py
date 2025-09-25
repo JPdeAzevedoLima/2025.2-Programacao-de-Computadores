ba0c8b744d25caf609c80cd941b212ef23b494c0
JPdeAzevedoLima <jpdeazevedolima@gmail.com>
2025-09-24 21:51:53 -0300

    Alterações

---
+++ 2025-09-24/Media_Aritmetica.py
@@ -0,0 +1,10 @@
+#Desenvolva um programa em Python que solicite ao usuário dois
+#valores inteiros e em seguida calcule a média aritmética entre eles.
+
+valorA = int(input('Informe o primeiro valor: '))
+
+valorB = int(input('Informe o segundo valor: '))
+
+media = (valorA + valorB) / 2
+
+print(f'A média aritmética é: {media:.1f}')
