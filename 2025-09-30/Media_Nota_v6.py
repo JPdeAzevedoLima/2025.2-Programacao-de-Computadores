ed026951ca19967933d8cf4357963724b3c14a29
JPdeAzevedoLima <jpdeazevedolima@gmail.com>
2025-09-30 21:51:23 -0300

    Modificações

--- 2025-09-30/Media_Nota_v6.py
+++ 2025-09-30/Media_Nota_v6.py
@@ -3,7 +3,11 @@
    no IFRN.
 
    Sabe-se que são duas notas e que o peso da primeira nota é 2
-   e o peso da segunda nota é 3. 
+   e o peso da segunda nota é 3 e que as notas são compreendidas entre
+   0 e 100.
+
+   Caso as notas não sejam válidas, deve-se informar ao usuário e sair do 
+   programa.
 
    Sabe-se também que para se calcular a frequência, deve-se informar a carga 
    horária da disciplina (em horas/aula) e a quantidade de faltas do aluno.
@@ -13,10 +17,21 @@
    - Teste 3: Nota1 = 72 e Nota2 = 29
    - Teste 4: Nota1 = 10 e Nota2 = 21
 '''
+import sys
 
-# Informando as notas como números inteiros
+# Informando a nota 1 como número inteiro
 intNota1 = int(input('Informe a nota da ETAPA 1: '))
+
+# Saindo do programa se a nota 1 for inválida
+if not (intNota1>=0 and intNota1<=100):
+   sys.exit('Nota 1 inválida! Deve ser entre 0 e 100.')
+
+# Informando a nota 2 como número inteiro
 intNota2 = int(input('Informe a nota da ETAPA 2: '))
+
+# Saindo do programa se a nota 2 for inválida
+if not (intNota2>=0 and intNota2<=100):
+   sys.exit('Nota 2 inválida! Deve ser entre 0 e 100.')
 
 # Informando a carga horária da disciplina (horas/aula)
 # e a quantidade de faltas do aluno
@@ -33,14 +48,16 @@ fltFrequencia = round( (1 - (intFaltas / intCargaHoraria)) * 100, 1)
 # Mostrando as notas e a média
 print(f'Nota da Etapa 1: {intNota1}')
 print(f'Nota da Etapa 2: {intNota2}')
-print(f'Média: {intMedia}')
+print(f'Média..........: {intMedia}')
 
 # Mostrar a Frequência
-print(f'Total de Aulas: {intCargaHoraria}')
+print(f'Total de Aulas.: {intCargaHoraria}')
 print(f'Total de Faltas: {intFaltas}')
-print(f'Frequência: {fltFrequencia}%')
+print(f'Frequência.....: {fltFrequencia}%')
 
 # Mostrando se o aluno está APROVADO, PROVA FINAL ou REPROVADO
+# TODO: implementar a lógica para mostrar se o aluno foi 
+# reprovado por nota, por frequência ou por ambos
 if (intMedia >= 60) and (fltFrequencia>=75):
    print('APROVADO')
 elif (intMedia >= 20) and (fltFrequencia>=75):
